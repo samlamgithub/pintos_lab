9c4d27e8f96e9f8e79df5ed8b91b759e108dba81
Jiahao Lin <jl6613@imperial.ac.uk>
2015-01-28 16:44:01 +0000

    finished task 0

---
+++ src/devices/build/threads/thread.d
@@ -0,0 +1,8 @@
+threads/thread.o: ../../threads/thread.c ../../lib/stddef.h \
+ ../../lib/stdio.h ../../lib/debug.h ../../lib/stdarg.h \
+ ../../lib/stdbool.h ../../lib/stdint.h ../../lib/kernel/stdio.h \
+ ../../lib/string.h ../../threads/../lib/kernel/list.h \
+ ../../threads/synch.h ../../lib/kernel/list.h ../../threads/thread.h \
+ ../../threads/../lib/debug.h ../../threads/interrupt.h \
+ ../../threads/palloc.h ../../threads/switch.h ../../threads/vaddr.h \
+ ../../threads/loader.h ../../threads/../lib/string.h
